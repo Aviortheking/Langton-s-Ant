1d878ca09a067f87df9ab29b922a7da86ad7894b
Avior <2bazert@gmail.com>
2018-02-01 11:09:36 +0000

    Update prgm.js

--- prgm.js
+++ prgm.js
@@ -1,8 +1,11 @@
-var AntElement = "table-0";
-
 var Prgm = {
+	element: "table",
+	setElement: function(elementName) {
+		this.element = elementName;
+		this.Ant.element= elementName;
+	},
 	init: function(element, width) {
-		table = "<table id="+AntElement+">";
+		table = "<table id="+this.element+">";
 		for (t = 0; t < width; t++) {
 			table += "<tr>";
 			for (i = 0; i < width; i++) {
@@ -13,35 +16,43 @@ var Prgm = {
 		element.innerHTML += table;
 	},
 	Ant: {
+		element: "table",
 		prepos: [0,0],
 		pos: [0,0],
 		setAt: function(line, col) {
 			col--;
 			line--;
 			//window.alert(col + ", " + line + ", " + AntElement);
-			document.getElementById(AntElement).childNodes[0].childNodes[col].childNodes[line].classList.add("ant");
-			
-		},
-		move: function() {
+			document.getElementById(this.element).childNodes[0].childNodes[col].childNodes[line].classList.add("ant");
 			pos = document.getElementsByClassName("ant")[0];
 			this.pos = [pos.getAttribute("line"), pos.getAttribute("col")];
-			if(this.prepos[0] == 0 && this.prepos[1] == 0) {
-				this.force(1, 0);
-			} else if(pos.classList.contains("black")) {
-				window.alert(this.prepos[0]+this.pos[0]+", "+this.prepos[1]+this.pos[1]);
-				window.alert(this.prepos);
-				//this.force();
-			} else {
+			this.prepos = this.pos;
+			this.prepos[0] = this.pos[0] - 1;
+		},
+		move: function() {
 				window.alert(parseInt(this.prepos[0])+parseInt(this.pos[0])+", "+(parseInt(this.prepos[1])+parseInt(this.pos[1])));
-				window.alert(this.prepos);
+
+			if(pos.classList.contains("black")) {
+				//window.alert(this.prepos[0]+this.pos[0]+", "+this.prepos[1]+this.pos[1]);
+				//window.alert(this.prepos);
+				this.a();
+			} else {
+				//window.alert(parseInt(this.prepos[0])+parseInt(this.pos[0])+", "+(parseInt(this.prepos[1])+parseInt(this.pos[1])));
+				//window.alert(this.prepos);
 			}
 			//window.alert(this.pos);
 		},
-		force: function(line, col) {
+		a: function(line, col) {
 			//window.alert(line+", "+col);
 			//window.alert((line+parseInt(this.pos[0]))+", "+(col+parseInt(this.pos[1])));
 			pos = document.getElementsByClassName("ant")[0].classList.remove("ant");
-			document.getElementById(AntElement).childNodes[0].childNodes[line+parseInt(this.pos[0])].childNodes[col+parseInt(this.pos[1])].classList.add("ant");
+			document
+				.getElementById(this.element)
+				.childNodes[0]
+				.childNodes[line-1+parseInt(this.prepos[0])]
+				.childNodes[col-1+parseInt(this.prepos[1])]
+				.classList
+				.add("ant");
 			this.prepos = this.pos;
 		},
 	}
